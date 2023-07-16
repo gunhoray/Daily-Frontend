b945b3b83d3da7247bdce457f68ef2459ae54a2b
SAHA-YJB <pkomom123@naver.com>
2023-07-16 21:50:12 +0900

    푸터/어바웃모달 완료

---
+++ src/1. mainpage/components/footer/styled/footerEl.js
@@ -0,0 +1,47 @@
+import { styled } from "styled-components";
+
+export const FooterSection = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 60;
+  background: white;
+  justify-content: center;
+  align-items: center;
+  gap: 60;
+  display: inline-flex;
+`;
+export const FooterContainer = styled.div`
+  width: 266px;
+  justify-content: center;
+  align-items: center;
+  gap: 60;
+  display: flex;
+  position: relative;
+`;
+export const FooterAboutUs = styled.div`
+  width: 100px;
+  align-self: stretch;
+  text-align: center;
+  color: black;
+  font-size: 20px;
+  font-family: Roboto;
+  font-weight: 700;
+  line-height: 28;
+  word-wrap: break-word;
+  margin-right: 20px;
+  cursor: pointer;
+  &:hover {
+    color: orange;
+  }
+`;
+export const FooterCopyRight = styled.div`
+  width: 100px;
+  align-self: stretch;
+  text-align: center;
+  color: black;
+  font-size: 20px;
+  font-family: Roboto;
+  font-weight: 700;
+  line-height: 28;
+  word-wrap: break-word;
+`;
